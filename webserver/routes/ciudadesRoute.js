4b54a19ce0540630554d734b2138aaf6e040c130
Federico Colangelo <federico.colangelo@semperti.com>
2017-03-25 14:55:38 -0300

    API: Se puede consultar por una ciudad espefica

--- webserver/routes/ciudadesRoute.js
+++ webserver/routes/ciudadesRoute.js
@@ -8,9 +8,26 @@ router.get('/ciudad', function(req, res) {
         if (err) {
             res.send(err);
         }
-        res.status(200).json(ciudades);
+        else {   
+            res.status(200).json(ciudades);
+        }
     })
 });
+
+router.get('/ciudad/:id_ciudad', function(req, res) {
+    Ciudad.findById(req.params.id_ciudad, function(err, ciudad) {
+        if (err) {
+            res.send(err);
+        }
+        else if (ciudad === null) {
+            res.status(404).json({"msj": "ciudad no encontrada"});
+        }
+        else {
+            res.status(200).json(ciudad);
+        }
+    });
+});
+        
 
 router.post('/ciudad', function(req, res) {
     console.log(req.body);
