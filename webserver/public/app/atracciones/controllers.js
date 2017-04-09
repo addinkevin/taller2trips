6ff29bcc3d557b4897008677eed46f21cc07b342
kevin <addinkevin@gmail.com>
2017-04-09 19:40:47 -0300

    #70 Agregado de search box al mapa.

--- webserver/public/app/atracciones/controllers.js
+++ webserver/public/app/atracciones/controllers.js
@@ -428,14 +428,73 @@ atraccionesApp.controller('atraccionesAddEditController',
 
             $scope.createMap = function() {
                 var map = new google.maps.Map(document.getElementById('map'), {
-                    zoom: 4,
+                    zoom: 13,
                     center: {lat: $scope.atraccion.lat, lng: $scope.atraccion.lng}
+                });
+
+                var input = document.getElementById('pac-input');
+                var searchBox = new google.maps.places.SearchBox(input);
+                map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+                // Bias the SearchBox results towards current map's viewport.
+                map.addListener('bounds_changed', function() {
+                    searchBox.setBounds(map.getBounds());
+                });
+
+                var markers = [];
+
+                searchBox.addListener('places_changed', function() {
+                    var places = searchBox.getPlaces();
+
+                    if (places.length == 0) {
+                        return;
+                    }
+
+
+                    // Clear out the old markers.
+                    markers.forEach(function(marker) {
+                        marker.setMap(null);
+                    });
+                    markers = [];
+
+                    // For each place, get the icon, name and location.
+                    var bounds = new google.maps.LatLngBounds();
+                    places.forEach(function(place) {
+                        if (!place.geometry) {
+                            console.log("Returned place contains no geometry");
+                            return;
+                        }
+                        var icon = {
+                            url: place.icon,
+                            size: new google.maps.Size(71, 71),
+                            origin: new google.maps.Point(0, 0),
+                            anchor: new google.maps.Point(17, 34),
+                            scaledSize: new google.maps.Size(25, 25)
+                        };
+
+                        // Create a marker for each place.
+                        markers.push(new google.maps.Marker({
+                            map: map,
+                            icon: icon,
+                            title: place.name,
+                            position: place.geometry.location
+                        }));
+
+                        if (place.geometry.viewport) {
+                            // Only geocodes have viewport.
+                            bounds.union(place.geometry.viewport);
+                        } else {
+                            bounds.extend(place.geometry.location);
+                        }
+                    });
+                    map.fitBounds(bounds);
                 });
 
                 var marker = new google.maps.Marker({
                     position: {lat: $scope.atraccion.lat, lng: $scope.atraccion.lng},
                     map: map,
-                    title: 'Click atraccion'
+                    title: 'Click atraccion',
+                    mapTypeId: 'roadmap'
                 });
 
                 map.addListener('click', function(event) {
