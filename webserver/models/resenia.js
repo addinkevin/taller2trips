e23c55b74c013429b7e3788aac9ecc71c84a937c
Fedecolangelo <federico.colangelo@outlook.com>
2017-04-18 21:40:36 -0300

    BACK: Creo modelo de resenias closes #94

---
+++ webserver/models/resenia.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var reseniaSchema = mongoose.Schema({
+    // mongoose automaticamente agrega el campo id que corresponde al _id de mongodb
+    id_usuario: String,
+    descripcion: String,
+    id_ciudad: String,
+    id_atraccion: String,
+    idioma: String,
+    calificacion: Number
+});
+
+module.exports = mongoose.model('Resenia', reseniaSchema);
