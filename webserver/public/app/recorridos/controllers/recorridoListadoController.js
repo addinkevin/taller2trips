57379cea8d78340af6f9a44dd99bc8a768f267a0
kevin <addinkevin@gmail.com>
2017-05-08 19:40:21 -0300

    Carousel #173 #164

--- webserver/public/app/recorridos/controllers/recorridoListadoController.js
+++ webserver/public/app/recorridos/controllers/recorridoListadoController.js
@@ -3,6 +3,9 @@ var recorridos = angular.module('tripsApp.recorridos');
 recorridos.controller('recorridoListadoController', [ '$scope', '$http', '$location', 'RecorridosService',
     function($scope, $http, $location, RecorridosService) {
         $scope.recorridos = [];
+        $scope.myInterval = 0;
+        $scope.noWrapSlides = false;
+        $scope.active = 0;
 
         $scope.deleteRecorrido = function (recorrido) {
             RecorridosService.deleteRecorrido(recorrido).then(function success(res) {
@@ -21,11 +24,10 @@ recorridos.controller('recorridoListadoController', [ '$scope', '$http', '$locat
         };
 
         $scope.getLinksImagenes = function(recorrido) {
-            console.log(recorrido);
             var links = [];
             for (var i = 0; i < recorrido.ids_atracciones.length; i++) {
                 var atraccion = recorrido.ids_atracciones[i];
-                links.push({src: $scope.getImgAtraccionUrl(atraccion), alt: "", caption: atraccion.nombre});
+                links.push({src: $scope.getImgAtraccionUrl(atraccion), alt: "", caption: atraccion.nombre, id: i});
             }
             return links;
         };
@@ -38,9 +40,18 @@ recorridos.controller('recorridoListadoController', [ '$scope', '$http', '$locat
             return '/api/atraccion/'+atraccion._id+'/imagen?filename='+atraccion.imagenes[0];
         };
 
+        var makeSlides = function() {
+            for (var i = 0; i < $scope.recorridos.length; i++) {
+                var recorrido = $scope.recorridos[i];
+                recorrido.slides = $scope.getLinksImagenes(recorrido);
+                console.log(recorrido.slides);
+            }
+        };
+
         var loadRecorridos = function() {
             RecorridosService.getRecorridos().then(function success(res) {
                 $scope.recorridos = res.data;
+                makeSlides();
             }, function error(res) {
 
             });
