f75746c57698ebd5366c5ac76d05d7dbaedf1379
Fedecolangelo <federico.colangelo@outlook.com>
2017-05-14 15:59:13 -0300

    Agrego modelo de visitados closes #137

---
+++ webserver/models/visitados.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var visitadoSchema = mongoose.Schema({
+    // mongoose automaticamente agrega el campo id que corresponde al _id de mongodb
+    id_usuario: {type: Schema.Types.ObjectId, ref: 'User'},
+    id_atraccion: {type: Schema.Types.ObjectId, ref: 'Atraccion'},
+});
+
+visitdoSchema.index({id_usuario: 1, id_atraccion: 1}, {unique: true});
+
+module.exports = mongoose.model('Visitado', visitadoSchema);
