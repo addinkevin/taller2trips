1784a31b7a7b77ff89046cbdcec3a7bfce74cf05
Fedecolangelo <federico.colangelo@outlook.com>
2017-05-28 12:16:09 -0300

    Reporte para atracciones favoritas closes #211

---
+++ webserver/routes/reportesRoute.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var Favorito = require('../models/favoritos');
+
+router.get('/reporte/atraccionesFavoritas', function(req, res) {
+    var doce_meses = 365*24*3600*1000;
+    var tiempo_inicio = Date.now() - doce_meses;
+    var fecha_inicio = new Date(tiempo_inicio);
+    Favorito.aggregate(
+        [
+            {$match: {id_atraccion: {$ne: null}, fecha: {$gte: fecha_inicio}  }},
+            {$group:
+                {
+                    _id: "$id_atraccion",
+                    id_atraccion: {$first: "$id_atraccion"},
+                    value: {$sum: 1}
+                }
+            },
+            {$sort: {value: -1}}
+        ], function(err, resultado) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                Favorito.populate(resultado, {path: "id_atraccion", select: "nombre"}, function(err, favoritos) {
+                    if (err) {
+                        res.send(err);
+                    }
+                    else {
+                        res.status(200).json(favoritos);
+                    }
+                });
+            }
+        }
+    );
+});
+
+module.exports = router;
